36f11557d0798b8badabb114ad040bba7e33cc60
modadiscord2 <46260742+modadiscord2@users.noreply.github.com>
2019-01-05 16:19:58 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("528913131951620097")
+setInterval(function() {
+channel.send(`-1*&^*&(*&*&)(*(*(&*(&*(^(*^&^(^&*(&*(&(*&(*&(*^*&%^%(*t^&*()&*()&_)&u(u&*(&(*&(*&*(&*(&(*&(*&*(&*(&(*`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
